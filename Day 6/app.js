b9466a2d4316a33fc3c7d6b1011966a2ed0373d9
its-prachi18 <prachinarkar18@gmail.com>
2025-03-27 18:50:23 +0530

    Day 6 files added

---
+++ Day 6/app.js
@@ -0,0 +1,33 @@
+
+const Groq = require('groq-sdk');//import groq-sdk
+require('dotenv').config();//import dotenv
+const express = require('express');//import express
+const app = express();
+const port = 3000;
+
+app.use(express.json());//parse json
+
+const groq = new Groq({ apiKey: process.env['GROQ_API_KEY'] });// 
+
+app.post('/ask',async (req,res,next)=>{
+    const chatCompletion = await groq.chat.completions.create({
+        messages: [
+          {
+            role: "user",
+            content: req.body.prompt,//user input
+          },
+        ],
+        model: "llama-3.3-70b-versatile",
+        max_tokens: 100,
+    });
+
+    res.status(200).json({
+        response: chatCompletion.choices[0].message.content
+    });//response from the model
+    
+      
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on  http://localhost:${port}`);
+})
